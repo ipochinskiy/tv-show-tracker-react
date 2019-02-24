bb9658ef65fb64ea78589846bcf4d6aeb404d027
Ilia Pochinskii <ilia.pochinskii@stayfriends.com>
2019-02-25 00:21:30 +0100

    use Link in the NavBar component

--- src/js/components/NavBar.js
+++ src/js/components/NavBar.js
@@ -1,13 +1,16 @@
 import React from 'react';
+import { Link } from "react-router-dom";
 import './NavBar.scss';
 
 const NavBar = () => (
     <div className="NavBar" role="navigation">
         <div className="NavBar-header">
-            <a className="NavBar-brand" href="/">
-                <span className="glyphicon glyphicon-film"></span>
-                Show<strong>Trackr</strong>
-            </a>
+            <Link to="/">
+                <div className="NavBar-brand" >
+                    <span className="glyphicon glyphicon-film"></span>
+                    &nbsp;Show<strong>Trackr</strong>
+                </div>
+            </Link>
         </div>
     </div>
 );
--- src/js/components/NavBar.test.js
+++ src/js/components/NavBar.test.js
@@ -1,17 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { shallow } from 'enzyme';
+import { MemoryRouter, Link } from 'react-router-dom';
+import { mount } from 'enzyme';
 import NavBar from './NavBar';
 
 describe('Component: NavBar', () => {
     it('should render the logo', () => {
-        const component = shallow(<NavBar />);
+        const component = mount(<MemoryRouter><NavBar /></MemoryRouter>);
 
         expect(component).toHaveText("ShowTrackr");
     });
 
     it('should render a link to the home page', () => {
-        const component = shallow(<NavBar />);
+        const component = mount(<MemoryRouter><NavBar /></MemoryRouter>);
 
         expect(component.find('a').first().props().href).toBe('/');
     });
