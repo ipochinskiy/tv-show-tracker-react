357e9ff931ca9b991821071a1e0ded1626e8fe04
Ilia Pochinskii <ilia.pochinskii@stayfriends.com>
2019-02-24 05:56:14 +0100

    remove unused code

--- src/js/components/App.js
+++ src/js/components/App.js
@@ -6,38 +6,16 @@ import NavBar from "./NavBar";
 import SearchForm from "./SearchForm";
 import ShowList from "./ShowList";
 
-const alphabet = [
-    '0-9', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L',
-    'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
-];
-
-const genreList = [
-    'Action', 'Adventure', 'Animation', 'Children', 'Comedy',
-    'Crime', 'Documentary', 'Drama', 'Family', 'Fantasy', 'Food',
-    'Home and Garden', 'Horror', 'Mini-Series', 'Mystery', 'News', 'Reality',
-    'Romance', 'Sci-Fi', 'Sport', 'Suspense', 'Talk Show', 'Thriller', 'Travel',
-];
-
 export class App extends React.Component  {
     constructor(props) {
         super(props);
         this.state = { selectedLetter: null, selectedGenre: null };
 
-        this.selectGenre = this.selectGenre.bind(this);
-        this.selectLetter = this.selectLetter.bind(this);
         this.onSubmitSearchQuery = this.onSubmitSearchQuery.bind(this);
     }
 
     componentDidMount() {
         this.props.appLoaded();
-    }
-
-    selectLetter(letter) {
-        this.setState({ selectedLetter: letter });
-    }
-
-    selectGenre(genre) {
-        this.setState({ selectedGenre: genre });
     }
 
     onSubmitSearchQuery(searchQuery) {
--- src/js/components/ShowList.js
+++ src/js/components/ShowList.js
@@ -4,12 +4,6 @@ import './ShowList.scss';
 
 const ShowList = ({ showList }) => (
     <div className="ShowList">
-        {/* <div className="ShowList-header">
-            <div className="ShowList-title">Top 12 Shows</div>
-            <div className="ShowList-search">
-                <input className="ShowList-search-input" type="text" placeholder="Search..." />
-            </div>
-        </div> */}
         <div className="ShowList-content">
             {showList.map(show => (
                 <ShowItemCompact key={show.id} showItem={show} />
