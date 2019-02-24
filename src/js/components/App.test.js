3d7f0e6b7b7887e23ee039367b4e7ad092f87171
Ilia Pochinskii <ilia.pochinskii@stayfriends.com>
2019-02-24 23:58:57 +0100

    remove unneeded tests for the App component, fix the deep render test

--- src/js/components/App.test.js
+++ src/js/components/App.test.js
@@ -1,29 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 import { shallow } from 'enzyme';
 import { App } from './App';
 import NavBar from './NavBar';
 import SearchForm from './SearchForm';
 import ShowList from './ShowList';
 
-const DEFAULT_PROPS = {
-    searchShow: jest.fn(),
-    appLoaded: jest.fn(),
-};
-
 describe('Component: App', () => {
     let props;
 
     beforeEach(() => {
-        props = {
-            ...DEFAULT_PROPS,
-        };
+        props = createComponentProps();
     });
 
     it('should deeply render without crashing', () => {
         const div = document.createElement('div');
+        const dummyStore = createStore(state => ({}));
+        const wrappedComponenet = (
+            <Provider store={dummyStore}>
+                <App {...props} />
+            </Provider>
+        );
 
-        ReactDOM.render(<App {...props} />, div);
+        ReactDOM.render(wrappedComponenet, div);
         ReactDOM.unmountComponentAtNode(div);
     });
 
@@ -38,52 +39,11 @@ describe('Component: App', () => {
 
         expect(props.appLoaded).toHaveBeenCalled();
     });
-
-    describe('with "showList" containing 0 items', () => {
-        beforeEach(() => {
-            props = {
-                ...DEFAULT_PROPS,
-                showList: [],
-                token: 'i bims 1 token vong auth her',
-            };
-        });
-
-        it('should render the search form', () => {
-            const component = shallow(<App {...props} />);
-
-            expect(component).toContainMatchingElement(SearchForm);
-        });
-
-        describe('and after the search form submits a query', () => {
-            beforeEach(() => {
-                const component = shallow(<App {...props} />);
-                component.find(SearchForm).simulate('submit', 'bazzinga!');
-            });
-
-            it('should call "searchShow" in props', () => {
-
-                expect(props.searchShow).toHaveBeenCalledWith('bazzinga!', 'i bims 1 token vong auth her');
-            });
-        });
-    });
-
-    describe('with "showList" containing 1 item', () => {
-        let showList;
-
-        beforeEach(() => {
-            showList = [
-                { id: 'The Big Bang Theory' },
-            ];
-            props = {
-                ...DEFAULT_PROPS,
-                showList,
-            };
-        });
-
-        it('should render the show list', () => {
-            const component = shallow(<App {...props} />);
-
-            expect(component).toContainReact(<ShowList showList={showList} />);
-        });
-    });
 });
+
+function createComponentProps(options) {
+    return {
+        appLoaded: jest.fn(),
+        ...options,
+    };
+}
